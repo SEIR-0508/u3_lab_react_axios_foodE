5e69bbe198ce5b0a602a25b0baad17a729290a29
Ron <ron_rose@me.com>
2023-06-28 11:28:18 -0400

    added slider component

--- FoodE/src/components/Home.jsx
+++ FoodE/src/components/Home.jsx
@@ -1,5 +1,5 @@
-import { Swiper, SwiperSlide } from 'swiper/react'
-import 'swiper/css';
+import React from 'react'
+import Slider from './Slider';
 
 const Home = () => {
     return (
@@ -9,20 +9,9 @@ const Home = () => {
 
     <h2 className='welcome-message'>Welcome to Boozy Coder</h2>
     <h3 className='welcome-message'>Where sloppy code is NOT a problem</h3>
-
-        <Swiper
-        spaceBetween={50}
-        slidesPerView={3}
-        onSlideChange={() => console.log('slide change')}
-        onSwiper={(swiper) => console.log(swiper)} >
-
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        ...
-      </Swiper>
-
+        
+        <Slider/>
+        
         </div>
 
 
