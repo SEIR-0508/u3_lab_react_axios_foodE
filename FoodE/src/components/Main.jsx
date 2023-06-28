afbaf79bc13eac20e0b34069cb061bbb62983d6a
Steven Underwood <stevenunderwood2@hotmail.com>
2023-06-28 09:28:50 -0500

    Added header and main to app.jsx

---
+++ FoodE/src/components/Main.jsx
@@ -0,0 +1,8 @@
+
+
+
+export default function Main () {
+    return (
+        <h1>Something extremely cool will go here!</h1>
+    )
+}
---
+++ FoodE/src/components/Nav.jsx
@@ -0,0 +1,83 @@
+
+
+
+export default function Nav () {
+    return (
+        <>
+            <div>
+                <h3>Filter By Glass:</h3>
+                <div>
+                    <ul>
+                        <li>Highball Glass</li>
+                        <li>Cocktail Glass</li>
+                        <li>Old-Fashioned Glass</li>
+                        <li>Whiskey Glass</li>
+                        <li>Collins Glass</li>
+                        <li>Pousse Cafe Glass</li>
+                        <li>Champagne Flute</li>
+                        <li>Wiskey Sour Glass</li>
+                        <li>Cordial Glass</li>
+                        <li>Brandy Snifter</li>
+                        <li>White Wine Glass</li>
+                        <li>Nick and Nora Glass</li>
+                        <li>Hurricane Glass</li>
+                        <li>Coffee Mug</li>
+                        <li>Shot Glass</li>
+                        <li>Jar</li>
+                        <li>Irish Coffee Cup</li>
+                        <li>Punch Bowl</li>
+                        <li>Pitcher</li>
+                        <li>Pint Glass</li>
+                        <li>Copper Mug</li>
+                        <li>Wine Glass</li>
+                        <li>Beer Mug</li>
+                        <li>Margarita/Coupette Glass</li>
+                        <li>Beer Pilsner</li>
+                        <li>Beer Glass</li>
+                        <li>Parfait Glass</li>
+                        <li>Mason Jar</li>
+                        <li>Margarita Glass</li>
+                        <li>Martini Glass</li>
+                        <li>Balloon Glass</li>
+                        <li>Coupe Glass</li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <h3>Filter By Spirit:</h3>
+                <div>
+                    <ul>
+                        <li>Gin</li>
+                        <li>Rum</li>
+                        <li>Whiskey</li>
+                        <li>Scotch</li>
+                        <li>Brandy</li>
+                        <li>Vodka</li>
+                        <li>Wine</li>
+                        <li>Schnapps</li>
+                        <li>Tequila</li>
+                        <li>Champagne</li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <h3>Filter By Category:</h3>
+                <div>
+                    <ul>
+                        <li>Ordinary Drink</li>
+                        <li>Cocktail</li>
+                        <li>Shake</li>
+                        <li>Other / Unknown</li>
+                        <li>Cocoa</li>
+                        <li>Shot</li>
+                        <li>Coffee / Tea</li>
+                        <li>Homemade Liqueur</li>
+                        <li>Punch / Party Drink</li>
+                        <li>Beer</li>
+                        <li>Soft Drink</li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+}
