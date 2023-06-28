2fa42e7138e2cc70d46c75f63589b5cabc13625a
Steven Underwood <stevenunderwood2@hotmail.com>
2023-06-28 10:11:42 -0500

    Css work for header

--- FoodE/src/components/Header.jsx
+++ FoodE/src/components/Header.jsx
@@ -1,11 +1,17 @@
 import Nav from './Nav'
+import logo from '../assets/logo.png'
 
 export default function Header () {
     return (
-        <>
-        <h1>Something</h1>
-        <Nav />
-        </>
+        <div>
+        <div className="header">
+            <img src={logo} className="logo"/>
+            <input type="search" className="search"/>
+        </div>
+        <div>
+            <Nav />
+        </div>
+        </div>
 
     )
 }
