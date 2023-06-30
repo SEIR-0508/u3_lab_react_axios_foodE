6de40b41d532a9f760e138bac7cc151fbb29e1c0
LowellC86 <lowellcaraballo@gmail.com>
2023-06-29 20:08:59 -0400

    done

--- foode_react/src/components/SearchForm.jsx
+++ foode_react/src/components/SearchForm.jsx
@@ -3,41 +3,46 @@ import { useState } from 'react'
 import axios from 'axios'
 export default function SearchForm(props) {
 
-const [searchTerm, setSearchTerm] = useState('')
+const initialState = {
+  input:""
+}
+const [searchTerm, setSearchTerm] = useState(initialState)
 
 const handleSubmit = (e) => {
-  e.preventdefault()
+  e.preventDefault()
   getSearch()
+  
 }
 
 const handleChange = (e) => {
-  setSearchTerm({ ...searchTerm, ['']: e.target.value  })
   
-  console.log(searchTerm.toLowerCase())
-props.setCocktail(props.cocktail.filter(drink=>drink.strDrink.toLowerCase().includes(searchTerm.toLowerCase())===true))
+  setSearchTerm({ ...searchTerm, [e.target.id]: e.target.value  })
+  
+  console.log(searchTerm.input.toLowerCase())
+
 };
 
 const getSearch = async () => {
   const response = await axios.get(
-    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm.input}`
   );
-setSearchTerm(response.data.drinks)
+props.setCocktail(response.data.drinks)
   console.log(response.data.drinks)
 };
 
 
   return (
     <section className='section search'>
-      <form className='search-form' >
+      <form className='search-form' onSubmit={handleSubmit}>
         <div className='form-control'>
           <label htmlFor='name'>Search For Your Happiness</label>
           <input 
             type='text'
             name='name'
-            id='name'
+            id='input'
             onChange={handleChange}
           />
-          <button onSubmit={handleSubmit}>Press Me, You Know You Wanna</button>
+          <button type="Submit">Press Me, You Know You Wanna</button>
         </div>
       </form> 
      
