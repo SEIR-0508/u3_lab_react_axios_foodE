70eed412c0dcc4c0d6c301a76dba32c41b645ffe
jyekang <janicekangaroo@gmail.com>
2023-08-04 11:59:13 -0700

    added back button and uom

--- food/src/Components/DrinkDetail.jsx
+++ food/src/Components/DrinkDetail.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 
 const DrinkDetail = ({drinks, setSearchQuery}) => {
@@ -17,18 +18,27 @@ const DrinkDetail = ({drinks, setSearchQuery}) => {
 
     return drink ? (
        <div className='drinkdetails'>
+        <div>
+        <NavLink to="/Drinks" className="back-link">Back</NavLink>
+        </div>
         <img src={drink.strDrinkThumb}/>
         <h3>{drink.strDrink}</h3>
         <p>{drink.strGlass}</p>
         <p>{drink.strInstructions}</p>
         <p><strong>Ingredients:</strong></p>
         <ul className='ingredients'>
-            {Object.entries(drink)
-            .filter(([key, value]) => key.startsWith('strIngredient') && value !== null)
-            .map(([key, value]) => (
-                <li key={key}>{value}</li>
-            ))}
-        </ul>
+        {Object.entries(drink)
+        .filter(([key, value]) => key.startsWith('strIngredient') && value !== null)
+        .map(([key, ingredient]) => {
+            const measureKey = `strMeasure${key.split('strIngredient')[1]}`;
+            const measurement = drink[measureKey];
+            return (
+                <li key={key}>
+                    {ingredient} {measurement && `(${measurement})`}
+                </li>
+            );
+        })}
+    </ul>
        </div>
         
       ) : null;
